4ba9f83aac44e783f4a5b00d47d38a45833fded0
new_user <nrustgen@gmail.com>
2023-02-19 00:19:10 -0800

    4. Exception handling and Spring REST [#504797]

--- src/main/java/ru/job4j/controller/PersonController.java
+++ src/main/java/ru/job4j/controller/PersonController.java
@@ -1,12 +1,20 @@
 package ru.job4j.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.domain.Person;
 import ru.job4j.service.PersonService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,12 +22,16 @@ import java.util.Optional;
 @RequestMapping("/person")
 public class PersonController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersonController.class.getSimpleName());
+
     private final PersonService persons;
     private final BCryptPasswordEncoder encoder;
+    private final ObjectMapper objectMapper;
 
-    public PersonController(final PersonService persons, BCryptPasswordEncoder encoder) {
+    public PersonController(final PersonService persons, BCryptPasswordEncoder encoder, ObjectMapper objectMapper) {
         this.persons = persons;
         this.encoder = encoder;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping("/all")
@@ -31,13 +43,24 @@ public class PersonController {
     public ResponseEntity<Person> findById(@PathVariable int id) {
         var person = this.persons.findById(id);
         return new ResponseEntity<Person>(
-                person.orElse(new Person()),
-                person.isPresent() ? HttpStatus.OK : HttpStatus.NOT_FOUND
+                person.orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "User with this id wasn't found. Please, check the input.")),
+                HttpStatus.OK
         );
     }
 
     @PostMapping("/sign-up")
     public ResponseEntity<Person> create(@RequestBody Person person) {
+        if (person.getLogin().length() < 3 || person.getLogin().length() > 15) {
+            throw new IllegalArgumentException("Invalid login. Login length should be 3 - 15 characters.");
+        }
+        if (!person.getPassword().contains("[A-Za-z0-9]+") || person.getPassword().length() < 4) {
+            throw new IllegalArgumentException("Password should contain only characters (at least 1 LowerCase and UpperCase"
+                                               + " and  at least one number. Also length can't be less than 4 symbols.");
+        }
+        if (person.getLogin() == null || person.getPassword() == null) {
+            throw new NullPointerException("Login and password can't be empty.");
+        }
         person.setPassword(encoder.encode(person.getPassword()));
         return new ResponseEntity<Person>(
                 this.persons.save(person),
@@ -47,6 +70,9 @@ public class PersonController {
 
     @PutMapping
     public ResponseEntity<Void> update(@RequestBody Person person) {
+        if (person == null) {
+            throw new IllegalArgumentException("Person cannot be empty.");
+        }
         Optional<Person> personsById = persons.findById(person.getId());
         if (personsById.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -65,5 +91,18 @@ public class PersonController {
         person.setId(id);
         this.persons.delete(person);
         return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    public void exceptionHandler(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() {
+            {
+                put("message", e.getMessage());
+                put("type", e.getClass());
+            }
+        }));
+        LOGGER.error(e.getLocalizedMessage());
     }
 }
