3162c76ec0133850f159d4bc049a71bcc59b977c
new_user <nrustgen@gmail.com>
2023-03-01 15:39:31 -0800

    1. Spring Liquibase [#302330]

--- src/main/java/ru/job4j/PersonsApplication.java
+++ src/main/java/ru/job4j/PersonsApplication.java
@@ -1,12 +1,30 @@
 package ru.job4j;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
-public class PersonsApplication {
+public class PersonsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PersonsApplication.class);
+	}
+
+	@Bean
+	public SpringLiquibase liquibase(DataSource ds) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+		liquibase.setDataSource(ds);
+		return liquibase;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonsApplication.class, args);
@@ -16,5 +34,4 @@ public class PersonsApplication {
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
 }
